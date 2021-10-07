2309bb8e15d268bb8a4ce1ea2629462719c7a896
prakharrai2512 <prakharrai2512@gmail.com>
2021-10-08 03:35:15 +0530

    g

--- Assignment 1/2.c
+++ Assignment 1/2.c
@@ -1,7 +1,25 @@
 #include <stdio.h>
 #include<stdlib.h>
+#include<string.h>
+
+char n1[32];
+char n2[32];
+
+
 
 
 int main()  {
-    
+   char input[80];
+   int bin[80];
+   for(int i=0;i<80;i++){
+       bin[i]=0;
+   }
+   scanf("%s",input);
+   int len = strlen(input);
+   int rem=0,cnt=0,temp;
+   for(int i=0;i<len;i++){
+       //printf("%d %c \n",len, input[i]);
+       bin[len-i-1] = ((int)input[i])-48;
+   }
+   
 }
